ea119c1b94461926455d4228723c672c99c39f33
wwesleyasn <wwesley@asnmail.com>
2022-01-21 15:26:53 -0500

    Updated one twenty one

---
+++ exercises/58 - Gallery/gallery.js
@@ -0,0 +1,7 @@
+// The idea is that we are creating a plugin to use on the page
+function Gallery(gallery) {}
+
+// Using the plugin on the page
+
+const gallery1 = Gallery(document.querySelector('.gallery1'));
+const gallery2 = Gallery(document.querySelector('.gallery2'));
