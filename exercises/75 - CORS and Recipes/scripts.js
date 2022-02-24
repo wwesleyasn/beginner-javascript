fb274de100a2a4a3a7b1868e4a255c15e0dcb03e
wwesleyasn <wwesley@asnmail.com>
2022-02-24 17:03:47 -0500

    Updated two twenty four

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,8 +1,49 @@
-const baseEndpoint = `http://wwww.recipepuppy.com/api`;
+const baseEndpoint = `https://recipes.beginnerjavascript.com/api`;
+const proxy = `https://cors-anywhere.herokuapp.com/`;
 
-async function fetchRecipes(query) {
-  const res = await fetch(`${baseEndpoint}?q=${query}`);
-  const data = await res.json();
+const form = document.querySelector(`form.search`);
+
+const recipesGrid = document.querySelector(`.recipes`);
+
+function displayRecipes(recipes) {
+  console.log(`Creating HTML`);
+  const html = recipes.map(
+    (recipe) => `<div class="recipe">
+    <h2>${recipe.title}</h2>
+    <p>${recipe.ingredients}</p>
+    ${
+      recipe.thumbnail &&
+      `<img src="${recipe.thumbnail}" alt="${recipe.title}" class="recipe-image"/>`
+    }
+    <a href="${recipe.href}">View Recipe</a>
+    </div>`
+  );
+  recipesGrid.innerHTML = html.join(``);
 }
 
-fetchRecipes(`pizza`);
+async function fetchRecipes(query) {
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  return data;
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const el = event.currentTarget;
+  console.log(form.query.value);
+  fetchAndDisplay(form.query.value);
+}
+
+async function fetchAndDisplay(query) {
+  // turn form off
+  form.submit.disabled = true;
+  // submit the search
+  const recipes = await fetchRecipes(query);
+  console.log(recipes);
+  // turn it back on
+  form.submit.disabled = false;
+  // display results
+  displayRecipes(recipes.results);
+}
+
+form.addEventListener(`submit`, handleSubmit);
