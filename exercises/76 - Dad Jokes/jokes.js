4d829ab54e1a57e23fe13f956befd4d1b30beb6d
wwesleyasn <wwesley@asnmail.com>
2022-02-25 16:19:59 -0500

    Updated two twenty five

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,8 @@
+const jokeButton = document.querySelector(`.getJoke`);
+const jokeButtonSpan = jokeButton.querySelector(`.jokeText`);
+const jokeHolder = document.querySelector(`.joke p`);
+const loader = document.querySelector(`.loader`);
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +13,38 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  // turn loader on
+  loader.classList.remove(`hidden`);
+  const response = await fetch(`https://icanhazdadjoke.com`, {
+    headers: {
+      Accept: `application/json`,
+    },
+  });
+  const data = await response.json();
+  // turn loader off
+  loader.classList.add(`hidden`);
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log(`Ahhh we used that one last time, look again`);
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  // using destructuring in this scenario. We only want the joke from the HTML
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButtonSpan.textContent = randomItemFromArray(
+    buttonText,
+    jokeButtonSpan.textContent
+  );
+}
+
+jokeButton.addEventListener(`click`, handleClick);
