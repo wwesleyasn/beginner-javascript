be157e7d2e4e60adfe41af7083a40ece7975cfa5
wwesleyasn <wwesley@asnmail.com>
2022-03-11 12:37:59 -0500

    Updated three eleven

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,3 +1,11 @@
+const fromSelect = document.querySelector(`[name = "from_currency"]`);
+const fromInput = document.querySelector(`[name = "from_amount"]`);
+const toSelect = document.querySelector(`[name = "to_currency"]`);
+const form = document.querySelector(`.app form`);
+const toEl = document.querySelector(`[name = "to_amount"]`);
+const endpoint = `https://api.exchangeratesapi.io/latest`;
+const ratesByBase = {};
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -32,3 +40,59 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+function generateOptions(options) {
+  return Object.entries(options)
+    .map(
+      ([currencyCode, currencyName]) =>
+        `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`
+    )
+    .join(``);
+}
+
+async function fetchRates(base = `USD`) {
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  // first check if we have the rates to convert from that currency
+  if (!ratesByBase[from]) {
+    console.log(
+      `Oh no, we don't have ${from} to convert to ${to}. So lets go get it!`
+    );
+    const rates = await fetchRates(from);
+    console.log(rates);
+    // store them for next time
+    ratesByBase[from] = rates;
+  }
+  // convert the amount being passed in
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+  return convertedAmount;
+}
+
+function formatCurrency(amount, currency) {
+  return Intl.NumberFormat(`en-US`, {
+    style: `currency`,
+    currency,
+  }).format(amount);
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(
+    fromInput.value,
+    fromSelect.value,
+    toSelect.value
+  );
+  toEl.textContent = formatCurrency(rawAmount, toSelect.value);
+}
+
+const optionsHTML = generateOptions(currencies);
+// populate the options elements
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
+
+form.addEventListener(`input`, handleInput);
